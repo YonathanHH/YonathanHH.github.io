f71a3646b9f98b4c2e58cecd72d7b9d1dbbc08f0
Yonathan Hary <yonathanhary1@gmail.com>
2026-04-07 12:58:46 +0700

    Add new website structure

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import CV from './pages/CV';
+import Biography from './pages/Biography';
+
+export default function App() {
+  return (
+    <Router>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/cv" element={<CV />} />
+            <Route path="/biography" element={<Biography />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
